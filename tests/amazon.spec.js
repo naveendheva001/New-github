ef5db84d00d1cff81a188cb61decaf4b0aaf0d68
naveendheva001 <naveendheva001@gmail.com>
2026-08-19 15:21:46 +0530

    serch_bar fill

--- tests/amazon.spec.js
+++ tests/amazon.spec.js
@@ -3,6 +3,7 @@ import{test} from "@playwright/test";
 test("amazon",async({page})=>{
 
     await page.goto("https://www.amazon.com/");
-   await page.locator("#twotabsearchtextbox");
+   await page.locator("#twotabsearchtextbox").fill("iphone");
+   await page.waitForTimeout(4000);
 
 })
