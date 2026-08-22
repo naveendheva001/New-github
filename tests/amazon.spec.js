d0a08303f0e0bcc2e679b077057a03d312bee0dd
naveendheva001 <naveendheva001@gmail.com>
2026-08-22 11:20:46 +0530

    data

--- tests/amazon.spec.js
+++ tests/amazon.spec.js
@@ -1,9 +1,21 @@
-import{test} from "@playwright/test";
+// import{test} from "@playwright/test";
 
-test("amazon",async({page})=>{
+// test("amazon",async({page})=>{
 
-    await page.goto("https://www.amazon.com/");
-   await page.locator("#twotabsearchtextbox").fill("iphone");
-   await page.waitForTimeout(4000);
+//     await page.goto("https://www.amazon.com/");
+//    await page.locator("#twotabsearchtextbox").fill("iphone");
+//    await page.waitForTimeout(4000);
+
+// })
+
+import { test } from '@playwright/test';
+
+test('myntra', async ({ page }) => {
+  await page.goto('https://www.myntra.com/boy-tshirts');
+
+  let counts = await page.locator('//li[@class="product-base"]');
+  let total_count =await counts.count();  
+
+console.log(await total_count);
 
 })
